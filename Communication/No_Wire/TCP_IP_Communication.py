cf975a4bf86f1bad9d669b82780cdcc4480b4bcf
Termuni <sungad9384@gmail.com>
2024-09-02 16:03:39 +0900

    #7.1 phk code Get
    1. 현규 무선통신 코드 획득, 일부 수정
    2. 테스트용 코드 수정 중

---
+++ Communication/No_Wire/TCP_IP_Communication.py
@@ -0,0 +1,24 @@
+import socket
+
+def Init_Socket(HOST = '0.0.0.0', PORT = 9091, s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)):
+    
+    # HOST = '0.0.0.0'  # 서버는 모든 IP에서 접속을 허용합니다.
+    # PORT = 9091  # 클라이언트와 동일한 포트 사용
+    # s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    s.bind((HOST, PORT))
+    s.listen(1)
+    print('Server listening...')
+
+    conn, addr = s.accept()
+    print('Connected by', addr)
+    return conn
+
+def Connect_Socket(conn):
+    data = conn.recv(1024)
+    print("Client: ",data.decode())
+    return data
+
+def Send_Socket(conn, data = "a"):
+    conn.sendall(data.encode())
+
+
